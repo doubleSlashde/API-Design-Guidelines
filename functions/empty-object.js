edac740b45e9e1de036d8f8e0b9baa08f3a824e4
root <root@miles.doubleslash.org>
2022-02-21 13:39:30 +0100

    reorganize files

--- functions/empty-object.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = ({security}) => {
-  if (Object.keys(security).length === 0){
-    return [{message: 'Security Requirement objects with no properties define no security'}];
-  }
-};
